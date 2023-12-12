4db2ac21a4b1e4d416105c00a8c4eebde2ad2aa0
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2023-12-11 21:12:22 -0300

    Add files via upload

---
+++ Task_WaitAll/Task WaitAll/Task WaitAll/Exemplos.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Task_WaitAll
+{
+    public class Exemplos
+    {
+        public static async Task EscreveCidades_async(string[] Cidades)
+        {
+            for (int i = 0; i < Cidades.Length; i++)
+            {
+                Console.WriteLine(Cidades[i]);
+                await Task.Delay(1000);
+            }
+        }
+
+        public static async Task EscrevePaises_async(string[] Paises)
+        {
+            for (int i = 0; i < Paises.Length; i++)
+            {
+                Console.WriteLine(Paises[i]);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ Task_WaitAll/Task WaitAll/Task WaitAll/Program.cs
@@ -0,0 +1,30 @@
+﻿using Task_WaitAll;
+
+try
+{
+    string[] cidades = ["1 - Nova York, Estados Unidos",
+        "2 - Tóquio, Japão",
+        "3 - Bay Area, Califórnia, Estados Unidos",
+        "4 - Londres, Reino Unido",
+        "5 - Singapura, Singapura"];
+
+    string[] Paises = ["1 - Estados Unidos", "2 - China", "3 - Japão", "4 - Alemanha", "5 - Índia"];
+
+    Task[] tasks =
+    [
+        Exemplos.EscreveCidades_async(cidades),
+        Exemplos.EscrevePaises_async(Paises)
+    ];
+
+    // Array de tasks + TimeOut que encerra as Tasks
+    if (Task.WaitAll(tasks, 6000))
+        Console.WriteLine("\n\nTasks completadas com sucesso!");
+    else
+        Console.WriteLine("\n\nNão foi possível completar as Tasks!");
+}
+catch (AggregateException ae)
+{
+    Console.WriteLine("\n\nNão foi possível completar as Tasks!");
+    foreach (var ex in ae.InnerExceptions)
+        Console.WriteLine($"\n\nErro inesperado: {ex.Message}");
+}
